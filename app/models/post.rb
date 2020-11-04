d3925e5ace1b9302a5c6006274c6b7fe2adcce21
letItCurl <rolandlopez.developer@gmail.com>
2020-11-04 09:24:47 +0700

    add method to the model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
   has_rich_text :body
+
+  def send_notifications
+    user = user_mentions
+    user.each do |user|
+      
+    end
+  end
+
+  def user_mentions
+    @user ||= body.body.attachments.select {|a| a.attachable.class == User}.map(&:attachable).uniq
+  end
 end
